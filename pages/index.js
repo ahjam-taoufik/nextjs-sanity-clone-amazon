50b3fcee169691479d3b7fbc71477a20c9dcc2f3
taoufik ahjam <taoufikahjam@gmail.com>
2022-03-10 16:33:34 +0100

    create layout

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 import { Typography } from '@mui/material'
 import Head from 'next/head'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div >
-      <Head>
-        <title>Ecommerce App</title>
-        <meta name="description" content="web Application for E-commerce" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-           <Typography component='h1' variant='h1' >Sanity Amazon</Typography>
-   
-       
-    </div>
+     <Layout>test </Layout>
   )
 }
