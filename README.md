823d6c447b81366306912c9e9510a53a1a3b0bb0
taoufik ahjam <taoufikahjam@gmail.com>
2022-03-10 20:10:11 +0100

    Create React Context and dark mode

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-Create Product Details Screen
-   1. add [slug].js to product folder
-   2. create component
-   3. get slug from the url
-   4. fetch product from sanity client
-   5. render product image, info
-   6. show add to cart button
+Create React Context
+   1. define Context, Store and reducer
+   2. set darkMode flag
+   3. use it on layout
+   
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import '../styles/globals.css';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
-// import { StoreProvider } from '../utils/Store';
-// import { SnackbarProvider } from 'notistack';
-// import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { StoreProvider } from '../utils/Store';
 
 const clientSideEmotionCache = createCache({ key: 'css' });
 
@@ -14,15 +12,9 @@ function MyApp({
 }) {
   return (
     <CacheProvider value={emotionCache}>
-      {/* <SnackbarProvider
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      > */}
-        {/* <StoreProvider> */}
-          {/* <PayPalScriptProvider deferLoading={true}> */}
-            <Component {...pageProps} />
-          {/* </PayPalScriptProvider> */}
-        {/* </StoreProvider> */}
-      {/* </SnackbarProvider> */}
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
     </CacheProvider>
   );
 }
